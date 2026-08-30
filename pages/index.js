1bfff8eb2c036e532e7789df0a7b6488344b52ae
pinnettusardo <113231901+pinnettusardo@users.noreply.github.com>
2026-08-30 18:17:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,14 @@ export default function Home({ strutture }) {
   return (
     <div>
       <nav className="navbar">
-        <div className="logo">🏡 Havenest</div>
+        <div className="logo">
+          <svg width="34" height="34" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <path d="M50 92 C50 92 6 62 6 32 C6 14 22 4 38 12 C45 16 50 24 50 24 C50 24 55 16 62 12 C78 4 94 14 94 32 C94 62 50 92 50 92 Z" fill="#d9532b"/>
+            <path d="M50 34 L70 62 L30 62 Z" fill="#ffffff"/>
+            <rect x="44" y="50" width="12" height="14" fill="#d9532b"/>
+          </svg>
+          Havenest
+        </div>
         <div className="navbar-right">
           <span className="nav-link">Diventa host</span>
           <button className="icon-btn">🌐</button>
