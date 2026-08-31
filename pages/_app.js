9f1ae87e764e8b7e0b4a76476a7a9c6e797b9c06
pinnettusardo <113231901+pinnettusardo@users.noreply.github.com>
2026-08-31 09:28:04 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <title>Havenest</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
